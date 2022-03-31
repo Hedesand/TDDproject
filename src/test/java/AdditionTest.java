ddeed3a065e2af84820b3272fe78efcb9fd15b48
gabriella <gabriella.hedesand@hotmail.com>
2022-03-31 11:37:36 +0200

    first commit

--- src/test/java/AdditionTest.java
+++ src/test/java/AdditionTest.java
@@ -1,16 +1,16 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AdditionTest {
     Calculator calculator;
 
     @BeforeEach
     void setUp()
-    {calculator = new Calculator();
-    }
+    {calculator = new Calculator();}
+
     @Test
     @DisplayName("Additions test")
     void addTwoNumbers() {
