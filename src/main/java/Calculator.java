1c67fa22c8f99ea6da44084f201819a110a48839
gabriella <gabriella.hedesand@hotmail.com>
2022-03-31 10:44:17 +0200

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+    public int add (int numberA, int numberB) {
+        return numberA + numberB;
+    }
+    public int sub (int numberA, int numberB) {
+        return numberA - numberB;
+    }
+    public int mul (int numberA, int numberB) {
+        return numberA * numberB;
+    }
+}
---
+++ src/test/java/AdditionTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class AdditionTest {
+    Calculator calculator;
+
+    @BeforeEach
+    void setUp()
+    {calculator = new Calculator();
+    }
+    @Test
+    @DisplayName("Additions test")
+    void addTwoNumbers() {
+        assertEquals(4, ((Calculator) calculator).add(2,2));
+    }
+
+    @Test
+    @DisplayName("Subtraktion test")
+    void subTwoNumbers() {
+        assertEquals(4, ((Calculator) calculator).sub(6,2));
+    }
+}
